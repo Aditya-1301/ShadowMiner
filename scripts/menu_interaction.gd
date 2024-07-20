588d02aee688cf7bf4716a13dc21536efa26b4d1
Aditya Gupta <adigup@kth.se>
2024-07-20 21:49:34 +0100

    Added player movement scripts and temporary sprites

--- scripts/menu_interaction.gd
+++ scripts/menu_interaction.gd
@@ -10,7 +10,5 @@ func _ready():
 func _process(delta):
 	pass
 
-
-
 func _on_mouse_entered():
 	pass # Replace with function body.
--- start_text.gd
+++
@@ -1,7 +0,0 @@
-extends Button
-
-@export var next_scene_path: String
-
-func _on_pressed():
-	get_tree().change_scene_to_file(next_scene_path)
-	
