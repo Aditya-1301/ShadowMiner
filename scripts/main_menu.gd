00babe0039c940e6b0775c43cc255aaf25f0938e
Aditya Gupta <adigup@kth.se>
2024-07-21 14:47:15 +0100

    Made slight change to move stage0.tscn to scenes folder
    so that clicking on the start button actually does something

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,10 @@
 extends MarginContainer
 
-@export var next_scene_path: String
-
+#@export var next_scene_path: String
 
 func _on_StartButton_pressed():
-	get_tree().change_scene_to_file(next_scene_path)
+	get_tree().change_scene_to_file("res://scenes/stage0.tscn")
+	#get_tree().change_scene_to_file(next_scene_path)
 
 func _on_QuitButton_pressed():
 	get_tree().quit(0)
