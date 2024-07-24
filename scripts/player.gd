58ee093c7c3b4d49920b7d4d927364ab9a9cb832
Aditya Gupta <adigup@kth.se>
2024-07-24 14:21:52 +0100

    can now enable/disable item attraction through mouse click

--- scripts/player.gd
+++ scripts/player.gd
@@ -7,6 +7,21 @@ const JUMP_VELOCITY = -400.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 @onready var animated_sprite_2d = $AnimatedSprite2D
+@onready var attraction_zone = $"Attraction Zone"
+
+
+func _ready():
+	attraction_zone.gravity_space_override = attraction_zone.SPACE_OVERRIDE_DISABLED
+
+func _input(_event):
+	if Input.is_action_just_pressed("staff_on"):
+		attraction_zone.gravity_space_override = attraction_zone.SPACE_OVERRIDE_COMBINE
+		attraction_zone.gravity_point = true
+		print("Staff Enabled!")
+	elif Input.is_action_just_pressed("staff_off"):
+		attraction_zone.gravity_space_override = attraction_zone.SPACE_OVERRIDE_DISABLED
+		attraction_zone.gravity_point = false
+		print("Staff Disabled!")
 
 
 func _physics_process(delta):
@@ -32,5 +47,8 @@ func _physics_process(delta):
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		animated_sprite_2d.play("idle")
+	
+	if Input.is_action_just_pressed("staff_on"):
+		animated_sprite_2d.play("attack1")
 
 	move_and_slide()
