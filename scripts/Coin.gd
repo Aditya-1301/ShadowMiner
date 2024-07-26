c5181819405973b98dc3f3e4c380dfc5a5fa85da
Lukas Lannge <lannge@kth.se>
2024-07-26 22:54:01 +0200

    Add first time
    Needs one last thing. Teleport the item randomly to another location in vicinity (resuing scenes rn!!)

---
+++ scripts/Coin.gd
@@ -0,0 +1,51 @@
+extends RigidBody2D
+
+
+var value = randi_range(50, 100)
+var player_in_area = false
+@onready var pickable_area = $pickable_area
+@onready var reappear_time = $reappear_time
+
+signal coin_collected
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	gravity_scale = 0
+	pickable_area.body_entered.connect(on_body_entered)
+	pickable_area.body_exited.connect(on_body_exited)
+	reappear_time.connect("timeout", on_reappear_timeout)
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if player_in_area:
+		print("hide")
+		hide_and_collect_coin()
+
+
+func hide_and_collect_coin():
+	hide()
+	# Turn off collsion with player (make player unable to scan this obejct)
+	set_collision_mask_value(4, false)
+	
+	# Use coin_collected signal to get the value of the coin!
+	coin_collected.emit(value)
+	
+	# Teleport to random location (in vicinity)
+	# TBD
+	
+	# Start time until to reappera
+	reappear_time.start()
+
+func on_body_entered(body: Node2D) -> void:
+	if body.name == str("Player"):
+		player_in_area = true
+		
+func on_body_exited(body: Node2D) -> void:
+	if body.name == str("Player"):
+		player_in_area = false
+
+func on_reappear_timeout() -> void:
+	# Change value to random value
+	value = randi_range(50, 100)
+	show()
