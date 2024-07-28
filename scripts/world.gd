28ac55b33896d8e49dadd34e678efc3e43664a23
Aditya Gupta <adigup@kth.se>
2024-07-28 16:25:36 +0100

    Added 1st end screen, and score display

--- scripts/world.gd
+++ scripts/world.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var hearts_container = $CanvasLayer/HeartsContainer
 @onready var player = $Player
+@onready var label = $CanvasLayer/Label
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +11,14 @@ func _ready():
 	$Music.play()
 	hearts_container.setMaxHearts(player.maxHealth)
 	hearts_container.updateHearts(player.currentHealth)
+	label.text = "Money: " + str(player.money_aquired)
 
+
+func _on_replay_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/world.tscn")
+
+func _on_main_menu_button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	
+func _process(_delta):
+	label.text = "Money: " + str(player.money_aquired)
